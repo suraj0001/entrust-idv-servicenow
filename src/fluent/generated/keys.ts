4a697914f5f5420697f4aa6829f0882a93d01848
Suraj Kumar <suraj.kumar@entrust.com>
2026-07-30 14:17:36 +0530

    test connection implementation

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -9,21 +9,9 @@ declare global {
                         table: 'sys_module'
                         id: 'b244e144f88e4f9faec902ed4d5d85ae'
                     }
-                    br0: {
-                        table: 'sys_script'
-                        id: '4337d190a4b14253835926bde3911477'
-                    }
-                    cs0: {
-                        table: 'sys_script_client'
-                        id: 'dc26586909924c90a966f0af47888fc2'
-                    }
                     package_json: {
                         table: 'sys_module'
                         id: '2eecee46c14c4d65bc53a45afe7b119b'
-                    }
-                    src_server_script_ts: {
-                        table: 'sys_module'
-                        id: '7783369edf86402b96193b992857021d'
                     }
                 }
             }
