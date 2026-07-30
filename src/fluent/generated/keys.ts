355eb0450532fb42c18bcdd6181c1c8c0d737b40
System Administrator <admin@example.com>
2026-07-30 15:30:05 +0530

    synced keys

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -9,9 +9,21 @@ declare global {
                         table: 'sys_module'
                         id: 'b244e144f88e4f9faec902ed4d5d85ae'
                     }
+                    'entrust-setup-ajax-si': {
+                        table: 'sys_script_include'
+                        id: '3fcd30784fcc48f991b8bab7e1daf362'
+                    }
                     package_json: {
                         table: 'sys_module'
                         id: '2eecee46c14c4d65bc53a45afe7b119b'
+                    }
+                    'src_server_script-includes_EntrustSetupAjax_js': {
+                        table: 'sys_module'
+                        id: '58a2a2edd7ad4b5c92fac5508f170583'
+                    }
+                    'src_server_setup_entrust-setup_ts': {
+                        table: 'sys_module'
+                        id: '2ab0663c5adc427d960caa451766b53c'
                     }
                 }
             }
