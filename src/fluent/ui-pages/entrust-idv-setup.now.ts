cf1e4ba73f1a8bf7b05b52d01c230b7f56e3c17a
Suraj Kumar <suraj.kumar@entrust.com>
2026-07-30 18:41:15 +0530

    direct true

--- src/fluent/ui-pages/entrust-idv-setup.now.ts
+++ src/fluent/ui-pages/entrust-idv-setup.now.ts
@@ -5,7 +5,7 @@ export const EntrustSetupPage = UiPage({
     $id: Now.ID['entrust-setup-ui-page'],
     endpoint: 'x_entru_entrustidv_entrust_idv_setup.do',
     description: 'Getting Started setup page for Entrust Identity Verification.',
-    direct: true,
+    direct: false,
     category: 'general',
     html: Now.include('../../server/ui-pages/entrust-idv-setup.html'),
     clientScript: Now.include('../../server/ui-pages/entrust-idv-setup.client.js'),
