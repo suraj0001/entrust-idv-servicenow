f24154fc24a8514bb3b4f118fac44537e80f1a14
Suraj Kumar <suraj.kumar@entrust.com>
2026-08-10 16:29:58 +0530

    show idv status

--- src/fluent/generated/data/table/incident.now.ts
+++ src/fluent/generated/data/table/incident.now.ts
@@ -1,20 +1,11 @@
-import { Table, ChoiceColumn, ReferenceColumn } from '@servicenow/sdk/core'
+import { Table, StringColumn } from '@servicenow/sdk/core'
 
 export const incident = Table({
     augments: 'incident',
     schema: {
-        x_entru_entrustidv_idv_status: ChoiceColumn({
-            dropdown: 'dropdown_with_none',
+        x_entru_entrustidv_idv_status: StringColumn({
             label: 'IDV Status',
             maxLength: 100,
-        }),
-        x_entru_entrustidv_idv_outcome: ChoiceColumn({
-            dropdown: 'dropdown_with_none',
-            label: 'IDV Outcome',
-        }),
-        x_entru_entrustidv_idv_verification: ReferenceColumn({
-            label: 'IDV Verification',
-            referenceTable: 'x_entru_entrustidv_verification_request',
         }),
     },
 })
